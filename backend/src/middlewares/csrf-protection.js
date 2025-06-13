b53c8c22b27eed2b9adadfdc4376b9f30801ade8
rasam <rassamrl@gmail.com>
2025-06-13 17:22:39 +0300

    had to disable csrf verification to focus on CRUD

--- backend/src/middlewares/csrf-protection.js
+++ backend/src/middlewares/csrf-protection.js
@@ -5,22 +5,22 @@ const csrfProtection = (req, res, next) => {
   const csrfToken = req.headers["x-csrf-token"];
   const accessToken = req.cookies.accessToken;
 
-  if (!csrfToken || typeof csrfToken !== "string") {
-    throw new ApiError(400, "Invalid csrf token");
-  }
+  // if (!csrfToken || typeof csrfToken !== "string") {
+  //   throw new ApiError(400, "Invalid csrf token");
+  // }
 
-  const decodedAccessToken = verifyToken(
-    accessToken,
-    env.JWT_ACCESS_TOKEN_SECRET
-  );
-  if (!decodedAccessToken || !decodedAccessToken.csrf_hmac) {
-    throw new ApiError(400, "Invalid csrf token");
-  }
+  // const decodedAccessToken = verifyToken(
+  //   accessToken,
+  //   env.JWT_ACCESS_TOKEN_SECRET
+  // );
+  // if (!decodedAccessToken || !decodedAccessToken.csrf_hmac) {
+  //   throw new ApiError(400, "Invalid csrf token");
+  // }
 
-  const hmacHashedCsrf = generateCsrfHmacHash(csrfToken);
-  if (decodedAccessToken.csrf_hmac !== hmacHashedCsrf) {
-    throw new ApiError(403, "Forbidden. CSRF token mismatch");
-  }
+  // const hmacHashedCsrf = generateCsrfHmacHash(csrfToken);
+  // if (decodedAccessToken.csrf_hmac !== hmacHashedCsrf) {
+  //   throw new ApiError(403, "Forbidden. CSRF token mismatch");
+  // }
 
   next();
 };
